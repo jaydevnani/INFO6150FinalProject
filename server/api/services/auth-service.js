5087a1d6a040e9d0c44f1dadcf8a6c3d545fb54d
jaydevnani <jdevnani@gmail.com>
2022-08-10 09:02:41 -0400

    Updating otp sender function
    Updating transporter as well as Mailoptions body

--- server/api/services/auth-service.js
+++ server/api/services/auth-service.js
@@ -28,25 +28,25 @@ export const findUser = async (email) => {
 // 6 digit otp will be sent
 export const sendOtp = async (userData) => {
   const otp = Math.floor(100000 + Math.random() * 900000);
-  // const transporter = nodemailer.createTransport({
-  //   service: "gmail",
-  //   auth: {
-  //     user: process.env.EMAIL,
-  //     pass: process.env.PASSWORD,
-  //   },
-  // });
+  let transporter = nodemailer.createTransport({
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
 
-  // const mailOptions = {
-  //   from: process.env.EMAIL,
-  //   to: userData.email,
-  //   bcc: "devnani.j@northeastern.edu",
-  //   subject: "One Time Password for Mobile Plan Finder",
-  //   text: `Dear ${
-  //     userData.firstName + " " + userData.lastName + ","
-  //   }\n\nThank you for choosing Mobile Plan Finder.\n\nUse the following OTP to complete your Sign Up procedure.\n\n${otp}\n\nRegards,\nTeam Reactors.`,
-  // };
+  const mailOptions = {
+    from: process.env.EMAIL,
+    to: userData.email,
+    bcc: "jdevnani@gmail.com",
+    subject: "One Time Password for WireFly",
+    text: `Dear ${
+      userData.firstName + " " + userData.lastName + ","
+    }\n\nThank you for choosing Mobile Plan Finder.\n\nUse the following OTP to complete your Sign Up procedure.\n\n${otp}\n\nRegards,\nTeam WireFly.`,
+  };
 
-  // const data = await transporter.sendMail(mailOptions);
+  const data = await transporter.sendMail(mailOptions);
   console.log("OTP:", otp);
+  console.log(data)
   return { otp };
 };
